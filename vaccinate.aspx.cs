2c3ada798d87ff32a99f21428b1646d8c610e2f7
uddesh154 <uddeshbhingarde@gmail.com>
2021-07-31 11:04:25 +0530

    Done

--- vaccinate.aspx.cs
+++ vaccinate.aspx.cs
@@ -26,7 +26,7 @@ namespace WebApplication7
         protected void Button1_Click(object sender, EventArgs e)
         {
             string vaccine = DropDownList1.Text;
-            
+            string ID = TextBox1.Text;
             string mail = TextBox2.Text;
             string mobile = TextBox3.Text;
             string date = TextBox4.Text;
@@ -105,6 +105,7 @@ namespace WebApplication7
         {
             string ID = TextBox1.Text;
             string[] Done =new string[7];
+            string[] vaccines = new string[] { "BCG","polio", "Rota_virus", "Hepatatis_A", "Booster","TT", "Influanzae" };
             int vd = 0;
             //TO check valied ID
             try
@@ -129,185 +130,33 @@ namespace WebApplication7
             }
 
             //TO check Vaccination Status
+            for (int i = 0; i < 7; i++)
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                    conn.Open();
+                    string checkuser = "select * from "+ vaccines[i] +" where id='" + ID + "'";
+                    SqlCommand cmd = new SqlCommand(checkuser, conn);
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if (rd.HasRows)
+                    {
+                        vd = 1;
+                        Done[i] = null;
 
-            try
-            {
-                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn.Open();
-                string checkuser = "select * from BCG where id='" + ID + "'";
-                SqlCommand cmd = new SqlCommand(checkuser, conn);
-                SqlDataReader rd = cmd.ExecuteReader();
-                if (rd.HasRows)
-                {
-                    vd = 1;
-                    Done[0] = null;
-                    
 
+                    }
+                    else
+                    {
+                        vd = 0;
+                        Done[i] = vaccines[i];
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    vd = 0;
-                    Done[0] = "BCG";
+                    Response.Write("error" + ex.ToString());
                 }
             }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-            try
-            {
-
-                SqlConnection conn1 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn1.Open();
-                string checkuser1 = "select * from polio where id='" + ID + "'";
-                SqlCommand cmd1 = new SqlCommand(checkuser1, conn1);
-                SqlDataReader rd1 = cmd1.ExecuteReader();
-                if (rd1.HasRows)
-                {
-                    vd = 1;
-                    Done[1] = null;
-
-                }
-                else
-                {
-                    vd = 0;
-                    Done[1] = "Polio";
-                    
-                }
-                conn1.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-            try
-            {
-                SqlConnection conn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn2.Open();
-                string checkuser2 = "select * from Rota_virus where id='" + ID + "'";
-                SqlCommand cmd2 = new SqlCommand(checkuser2, conn2);
-                SqlDataReader rd2 = cmd2.ExecuteReader();
-                if (rd2.HasRows)
-                {
-                    vd = 1;
-                    Done[2] = null;
-
-                }
-                else
-                {
-                    vd = 0;
-                    Done[2] = "Rota_Virus";
-                    
-                }
-                conn2.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-            try
-            {
-
-                SqlConnection conn3 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn3.Open();
-                string checkuser3 = "select * from Hepatatis_A where id='" + ID + "'";
-                SqlCommand cmd3 = new SqlCommand(checkuser3, conn3);
-                SqlDataReader rd3 = cmd3.ExecuteReader();
-                if (rd3.HasRows)
-                {
-                    vd = 1;
-                    Done[3] = null;
-
-                }
-                else
-                {
-                    vd = 0;
-                    Done[3] = "Hepatatis_A";
-
-                    
-                }
-                conn3.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-            try
-            {
-                SqlConnection conn4 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn4.Open();
-                string checkuser4 = "select * from Booster where id='" + ID + "'";
-                SqlCommand cmd4 = new SqlCommand(checkuser4, conn4);
-                SqlDataReader rd4 = cmd4.ExecuteReader();
-                if (rd4.HasRows)
-                {
-                    vd = 1;
-                    Done[4] = null;
-                    
-
-                }
-                else
-                {
-                    vd = 0;
-                    Done[4] = "Booster";
-                }
-                conn4.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-            try
-            {
-                SqlConnection conn5 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn5.Open();
-                string checkuser5 = "select * from TT where id='" + ID + "'";
-                SqlCommand cmd5 = new SqlCommand(checkuser5, conn5);
-                SqlDataReader rd5 = cmd5.ExecuteReader();
-                if (rd5.HasRows)
-                {
-                    vd = 1;
-                    Done[5] = null;
-                    
-
-                }
-                else
-                {
-                    vd = 0;
-                    Done[5] = "TT";
-                }
-                conn5.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-            try
-            {
-                SqlConnection conn6 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                conn6.Open();
-                string checkuser6 = "select * from Influanzae where id='" + ID + "'";
-                SqlCommand cmd6 = new SqlCommand(checkuser6, conn6);
-                SqlDataReader rd6 = cmd6.ExecuteReader();
-                if (rd6.HasRows)
-                {
-                    vd = 1;
-                    Done[6] = null;
-
-                }
-                else
-                {
-                    vd = 0;
-                    Done[6] = "Influanzae";
-                    
-                }
-                conn6.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
-            }
-
             if(vd==1)
             {
                 Errorlabel.Text = "Vaccination done";
